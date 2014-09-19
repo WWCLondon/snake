357c0c09d2fb5bb600e2348b0c357cae73c88599
thattommyhall <thattommyhall@gmail.com>
2014-09-19 20:07:02 +0100

    init

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject snake "0.1.0-SNAPSHOT"
+  :description "Basic project to play snake"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [quil "2.2.2"]])
