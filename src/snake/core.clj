3e7d47bf34b1d017f4b2e24942d14a96ce4bfae0
thattommyhall <thattommyhall@gmail.com>
2014-09-20 13:52:53 +0100

    put everything back into core
    (figured out how to live-reload in Lighttable)

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -1,11 +1,85 @@
 (ns snake.core
-  (:require [quil.core :as q]
-            [snake.dynamic :as dynamic :refer [window-size key-pressed]]))
+  (:require [quil.core :as q]))
 
-(q/defsketch snake
+(def red 1)
+(def orange 25)
+(def yellow 50)
+(def green 100)
+(def blue 150)
+(def purple 200)
+
+(def window-size 800)
+(def grid-size 50)
+(def cell-size (/ window-size grid-size))
+
+(def snake (atom nil))
+
+(defn reset-snake []
+  (reset! snake
+          [[0 0]
+           [0 1]
+           [0 2]
+           [0 3]
+           [0 4]
+           [0 5]
+           [0 6]
+           ]))
+
+(reset-snake)
+
+(defn +mod [x y]
+  (mod (+ x y) grid-size))
+
+(defn vec+ [[x1 y1]
+            [x2 y2]]
+  [(+mod x1 x2) (+mod y1 y2)])
+
+(def up [0 -1])
+(def down [0 1])
+(def left [-1 0])
+(def right [1 0])
+(def direction (atom up))
+
+(defn update-snake [snake direction]
+  (concat (rest snake)
+          [(vec+ (last snake) direction)]))
+
+(defn move [snake direction]
+  (swap! snake update-snake direction))
+
+(defn draw-cell [[x y] hue]
+  (q/fill (q/color hue 255 255))
+  (q/rect (* cell-size x) (* cell-size y) cell-size cell-size))
+
+(defn setup []
+  (q/smooth)
+  (q/color-mode :hsb)
+  (q/frame-rate 12))
+
+(def direction-from-key {
+                         \w up
+                         \s down
+                         \a left
+                         \d right})
+
+(defn key-pressed []
+  (let [key (q/raw-key)]
+    (println key)
+    (if-let [new-direction (direction-from-key key)]
+      (reset! direction new-direction))))
+
+(defn draw []
+  (q/background 0)
+  (move snake @direction)
+  (dosync
+   (doseq [cell @snake]
+     (draw-cell cell red))))
+
+
+(q/defsketch snake-game
   :title "Snake"
-  :setup dynamic/setup
-  :draw dynamic/draw
+  :setup setup
+  :draw draw
   :size [window-size window-size]
   :key-typed key-pressed)
 
