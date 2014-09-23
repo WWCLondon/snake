5e9bfd9cfc25a1feb27262922fdc3d02b532a43a
winnab <winna.bridgewater@mammal.io>
2014-09-23 21:35:07 +0100

    update draw functioin to draw coordinates for pre-defined snake object

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -8,9 +8,12 @@
 (def blue 150)
 (def purple 200)
 
-(def window-size 800)
+(def window-size 200)
 (def grid-size 10)
 (def cell-size (/ window-size grid-size))
+(def snake { :color blue
+             :bodyparts [[3 2 ][2 2][1 2]]
+            })
 
 (defn draw-cell [[x y] hue]
   (q/fill (q/color hue 255 255))
@@ -21,11 +24,25 @@
   (q/color-mode :hsb)
   (q/frame-rate 1))
 
+
+;(def v1 [1 3])
+
+;(def v2 [3 4])
+
+
+;(defn vec+ [[x1 y1] [x2 y2]]
+;  [(+ x1 x2) (+ y1 y2)])
+
 (defn draw []
   (q/background 0)
-  (draw-cell [1 2] red)
-  (draw-cell [1 3] red)
-  ; Add stuff here
+
+
+
+  (let [color (snake :color)]
+    (doseq [x (snake :bodyparts)]
+      (draw-cell x color )))
+
+  ;; Add stuff here
   )
 
 (q/defsketch snake-game
